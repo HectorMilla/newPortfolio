a618cc60af58cc3c69e1830afd426d02d3fb9be8
hector milla <hectormilla8@gmail.com>
2018-09-25 20:46:52 -0400

    added some mobile functionality

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,19 +1,19 @@
-// js for story generator
+const explore = document.getElementById("explore");
 
-const activities = ["Reading", "Hiking", "Watching Netflix"];
-const verbs = ["spending time", "relaxing", "dancing"];
-const nouns = ["friends", "cat", "family"];
+explore.addEventListener("click", () => {
+  window.scrollTo({
+    top: 650,
+    behavior: "smooth"
+  });
+});
 
-const random = phrase => Math.floor(Math.random() * phrase);
-const writeStory = () => {
-  const activity = document.getElementById("empty-activity");
-  const verb = document.getElementById("empty-verb");
-  const noun = document.getElementById("empty-noun");
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+});
 
-  activity.innerHTML = activities[random(activities.length)];
-  activity.classList.add("random");
-  verb.innerHTML = verbs[random(verbs.length)];
-  verb.classList.add("random");
-  noun.innerHTML = nouns[random(nouns.length)];
-  noun.classList.add("random");
-};
+const header = document.getElementsByTagName("header")[0];
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 848) {
+  }
+});
